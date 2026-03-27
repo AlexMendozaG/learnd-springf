81340b1fc402f7959d0c1bf860bcb1c025b8cd61
alexander mendoza <alexandermendozaa08@gmail.com>
2026-03-26 18:04:58 -0600

    add first aspect app

---
+++ springf6_aspect/src/main/java/main/Main.java
@@ -0,0 +1,20 @@
+package main;
+
+import config.ProjectConfig;
+import models.Comment;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import services.CommentService;
+
+public class Main {
+    public static void main(String[] args) {
+
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+        CommentService service = context.getBean(CommentService.class);
+
+        Comment comment = new Comment();
+        comment.setText("Hello World");
+        comment.setAuthor("John Doe");
+
+        service.publicComment(comment);
+    }
+}
